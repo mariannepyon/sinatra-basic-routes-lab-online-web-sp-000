582b34744d3c39f0e16d2b0856fc55de5ff81343
Marianne Pyon <mariannepyon@gmail.com>
2019-11-08 20:38:40 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-end
+  get '/' do 
+    "Hello, World!"
+    end
+
+    get '/name' do
+      "My name is Marianne"
+    end
+
+    get '/hometown' do
+      "My hometown is Van Nuys"
+    end
+
+    get '/favorite-song' do
+      "My favorite song is Heart of God"
+    end
+  end
