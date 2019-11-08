79b34e87990dac67a233cde601605a05246ebaba
Marianne Pyon <mariannepyon@gmail.com>
2019-11-08 20:38:10 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,4 +3,16 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   get '/' do 
     "Hello, World!"
-end
+
+    get '/name' do
+      "My name is Marianne"
+    end
+
+    get '/hometown' do
+      "My hometown is Van Nuys"
+    end
+
+    get '/favorite-song' do
+      "My favorite song is Heart of God"
+    end
+  end
